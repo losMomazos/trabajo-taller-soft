68afc9ac50907ce8071f7513565ed9953603af33
Francisco Antilef <francisco.antilef.jaramillo@gmail.com>
2018-06-27 19:59:09 -0400

    Termina caso de uso filtrado
    	la app logra filtrar tanto en cpu,gpu y mother
    	pd:aplicacion poco mantenible

--- client/src/app/components/cpu/cpu.component.ts
+++ client/src/app/components/cpu/cpu.component.ts
@@ -8,14 +8,84 @@ import {Cpu} from '../../Cpu';
 })
 export class CpuComponent implements OnInit {
   cpus:Cpu[];
+  cpusFilter:Cpu[];
+  params:any=[];
+  actualParams:string="";
+  actualSocket:string="";
+  actualFab:string="";
+  actualCore:string="";
   constructor(private cpuService:CpuService) {
     this.cpuService.getCpus()
     .subscribe(cpus=>{
-      this.cpus = cpus;
+      this.cpusFilter = cpus;
+      this.cpus =  this.cpusFilter;
     })
   }
 
   ngOnInit() {
   }
 
+  search(param){
+    this.cpusFilter = this.cpus.filter(cpu=>{
+      return cpu.name.toLocaleLowerCase().includes(param.toLocaleLowerCase());
+    })
+  }
+  
+  select($event){
+    if($event.target.id=='socketSelection'){
+      if(this.actualParams!=""){
+        var a =0;
+        while(a<this.params.length && this.params[a].socket!=this.actualParams){
+          a++;
+        }     
+        this.params.splice(a,1);
+        this.actualParams ="";
+        if($event.target.value==""){
+          this.parameters();
+        }
+      }if($event.target.value!="" && this.actualParams==""){
+        this.actualParams = $event.target.value;
+        this.params.push({"socket":this.actualParams});
+        this.parameters();
+      }
+    }else if($event.target.id=="fabSelection"){
+      if(this.actualFab!=""){
+        var a =0;
+        while(a<this.params.length && this.params[a].fab!=this.actualFab){
+          a++;
+        }     
+        this.params.splice(a,1);
+        this.actualFab ="";
+        if($event.target.value==""){
+          this.parameters();
+        }
+      }if($event.target.value!="" && this.actualFab==""){
+        this.actualFab = $event.target.value;
+        this.params.push({"fab":this.actualFab});
+        this.parameters();
+      }
+    }else if($event.target.id=="coreSelection"){
+      if(this.actualCore!=""){
+        var a =0;
+        while(a<this.params.length && this.params[a].cores!=this.actualCore){
+          a++;
+        }     
+        this.params.splice(a,1);
+        this.actualCore ="";
+        if($event.target.value==""){
+          this.parameters();
+        }
+      }if($event.target.value!="" && this.actualCore==""){
+        this.actualCore = $event.target.value;
+        this.params.push({"cores":parseInt(this.actualCore)});
+        this.parameters();
+      }
+    }
+  }
+  parameters(){
+    this.cpuService.getCpuParameters(this.params)
+    .subscribe(cpus=>{
+      this.cpusFilter =  cpus;
+    })
+  }
 }
--- client/src/app/services/cpu.service.ts
+++ client/src/app/services/cpu.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Cpu} from '../Cpu';
 import {Gpu} from '../Gpu';
 import {Motherboard} from '../Motherboard';
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpParams} from '@angular/common/http'
 import {map} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
@@ -22,6 +22,20 @@ export class CpuService {
   }
   getMother(id){
     return this.http.get<Motherboard[]>(`${this.domain}/api/cpu/compatibilidadmother/${id}`)
+    .pipe(map(res=>res));
+  }
+  getCpuParameters(_params:any[]){
+    let params = new HttpParams();
+    for(var i=0;i<_params.length;i++){
+      if(_params[i].socket){
+        params = params.set('socket',_params[i].socket);
+      }else if(_params[i].fab){
+        params = params.set('fab',_params[i].fab);
+      }else if(_params[i].cores){
+        params = params.set('nucleos',_params[i].cores);
+      }
+    }
+    return this.http.get<Cpu[]>(`${this.domain}/api/cpu`,{params:params})
     .pipe(map(res=>res));
   }
 }
